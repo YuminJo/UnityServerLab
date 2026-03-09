e37c64f0546837281456b10e6a5dac380c543c3a
YuminJo <nationmilo@gmail.com>
2026-03-10 00:54:13 +0900

    feat: add base code for the project

--- BasicC#ServerStudy/DummyClient/Program.cs
+++ BasicC#ServerStudy/DummyClient/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DummyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
